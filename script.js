2b898ee5e63432ecdb63d3230636e81d390322c1
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-29 17:15:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let button = document.querySelector("input");
+let selectmain = document.querySelector("#colorSelect");
+button.addEventListener("click", remove)
+function remove(e) {
+	let optiontoremove=selectmain.options[selectmain.selectedIndex];
+	selectmain.removeChild(optiontoremove);
+}
+
